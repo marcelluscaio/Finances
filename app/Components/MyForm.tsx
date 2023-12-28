c08fd0decb2e516eb16f82e745ceb77705bca36a
Caio Cabral <ocaiocabral@gmail.com>
2023-12-27 19:22:23 -0500

    feat: form update

--- app/Components/MyForm.tsx
+++ app/Components/MyForm.tsx
@@ -3,17 +3,25 @@ import * as Form from "@radix-ui/react-form";
 
 const MyForm = () => (
 	<Form.Root>
-		<Form.Field name="Oi">
-			<Form.Label>Teste</Form.Label>
+		<Form.Field name="evento">
+			<Form.Label>Evento</Form.Label>
 			<Form.Control />
 			{/* <Form.Message /> */}
 			{/* <Form.ValidityState  /> */}
+		</Form.Field>
+		<Form.Field name="valor">
+			<Form.Label>Valor</Form.Label>
+			<Form.Control />
+		</Form.Field>
+		<Form.Field name="origem">
+			<Form.Label>Origem</Form.Label>
+			<Form.Control />
 		</Form.Field>
 
 		{/* <Form.Message /> */}
 		{/* <Form.ValidityState  /> */}
 
-		<Form.Submit />
+		<Form.Submit>Inserir</Form.Submit>
 	</Form.Root>
 );
 
